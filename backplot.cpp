07a92ec88ffe122618925266860d2ff4cd6e76ce
Nicholas Gill <mythagel@gmail.com>
2014-12-25 20:25:29 +1100

    Pre-openscenegraph

--- backplot.cpp
+++ backplot.cpp
@@ -1,27 +1,124 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+#include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Basic");
+struct vector_2 {
+    double x = 0.0;
+    double y = 0.0;
 
-    std::vector<sf::Vertex> vertices;
-    vertices.push_back(sf::Vector3f(0,0,0));
-    vertices.push_back(sf::Vector3f(10,0,0));
-    vertices.push_back(sf::Vector3f(10,10,0));
-    vertices.push_back(sf::Vector3f(0,10,0));
+    vector_2() = default;
+    vector_2(double x, double y)
+     : x(x), y(y) {
+    }
+};
+struct vector_3 {
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+
+    vector_3() = default;
+    vector_3(double x, double y, double z)
+     : x(x), y(y), z(z) {
+    }
+};
 
 
-    while (window.isOpen())
+int main() {
+    double fov = 120.0;
+    double zNear = 0.1;
+    double zFar = 100.0f;
+
+    vector_3 eye{0,0,10};
+    vector_3 center;
+    vector_3 up{0,0,1};
+
+    sf::Window window(sf::VideoMode(800, 600), "SFML Basic");
+    window.setVerticalSyncEnabled(true);
+
+    // Enable Z-buffer read and write
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearDepth(1.f);
+
+    // Reshape
     {
+        auto x = window.getSize().x;
+        auto y = window.getSize().y;
+        auto aspect = x/y;
+
+        glViewport(0, 0, x, y);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(fov, aspect, zNear, zFar);
+        glMatrixMode(GL_MODELVIEW);
+    }
+
+
+    for(bool running = true; running; ) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+        while (window.pollEvent(event)) {
+            switch(event.type) {
+                case sf::Event::Closed:
+                    running = false;
+                    break;
+                case sf::Event::Resized: {
+                    auto x = event.size.width;
+                    auto y = event.size.height;
+                    auto aspect = x/y;
+
+                    glViewport(0, 0, x, y);
+                    glMatrixMode(GL_PROJECTION);
+                    glLoadIdentity();
+                    gluPerspective(fov, aspect, zNear, zFar);
+                    glMatrixMode(GL_MODELVIEW);
+                    break;
+                }
+                case sf::Event::KeyPressed: {
+                    std::cout << "key: " << event.key.code << "\n";
+                    std::cout << "control:" << event.key.control << "\n";
+                    std::cout << "alt:" << event.key.alt << "\n";
+                    //if (event.key.code == sf::Keyboard::Escape)
+                    break;
+                }
+                case sf::Event::MouseWheelMoved: {
+                    std::cout << "wheel movement: " << event.mouseWheel.delta << std::endl;
+                    std::cout << "mouse x: " << event.mouseWheel.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseWheel.y << std::endl;
+                    break;
+                }
+                case sf::Event::MouseButtonPressed: {
+                    if (event.mouseButton.button == sf::Mouse::Right) {
+                        std::cout << "the right button was pressed" << std::endl;
+                        std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                        std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                    }
+                    break;
+                }
+                case sf::Event::MouseMoved: {
+                    std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
+                    std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+                    break;
+                }
+            }
         }
 
-        window.clear();
-        window.draw(&vertices[0], vertices.size(), sf::Lines);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glLoadIdentity();
+        gluLookAt(eye.x, eye.y, eye.z, center.x, center.y, center.z, up.x, up.y, up.z);
+
+        glTranslatef(1.5f, 0.0f, -7.0f);
+        glLineWidth(10.0);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glBegin(GL_LINES);
+            glVertex3f(0,0,-1);
+            glVertex3f(10,0,-1);
+            glVertex3f(10,10,-1);
+            glVertex3f(0,10,-1);
+        glEnd();
+
+        glFlush();
+
         window.display();
     }
 
